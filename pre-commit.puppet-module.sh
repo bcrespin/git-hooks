144a026d6bec8c69603c270b0923c1da0411ec7f
Brice C <maxpayne911@hotmail.com>
2016-05-25 10:00:19 +0200

    added option to puppet-lint

--- pre-commit.puppet-module.sh
+++ pre-commit.puppet-module.sh
@@ -3,6 +3,7 @@
 # https://www.chriscowley.me.uk/blog/2014/06/25/super-slick-agile-puppet-for-devops/
 # pre-commit git hook to check the validity of  modules manifest/yaml
 
+PUPPETLINT_OPTS=" --no-80chars-check "
 
 echo "******************************************************"
 echo "*** INFO Running pre-commit script..."
@@ -10,7 +11,7 @@ echo "*** INFO Running pre-commit script..."
 for file in `git diff --name-only --cached | grep -E '\.(pp)'`; do
 
   echo "*** INFO Checking style on $file : "
-  puppet-lint ${file}
+  puppet-lint $PUPPETLINT_OPTS  ${file}
   ret=$?
   if [ $ret -ne 0 ]; then
      echo "**** ERROR aborting the commit";
